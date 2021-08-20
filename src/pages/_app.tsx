799be58e860f087d8a2ea642bf0cd7c33ffca961
Yury-Tomaz <yuri.tomaz@wexark.com.br>
2021-08-20 11:33:19 -0300

    add btn whatsapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { Whatsapp } from '../components/Whatsapp'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
      <>
      <Header />
+     <Whatsapp/>
      <Component {...pageProps} />
     </>
     )
